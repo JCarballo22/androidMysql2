2a76db68464a93c96c4fae41a57d41c1b60da084
Jonathan Carballo <jonathanfrancisco.carballo@unab.edu.sv>
2022-11-22 20:25:10 -0600

    22/11/2022

--- app/src/main/java/com/example/androidmysql2/RegistrosActivity.kt
+++ app/src/main/java/com/example/androidmysql2/RegistrosActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidmysql2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,6 +10,7 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 
 class RegistrosActivity : AppCompatActivity() {
@@ -30,16 +32,20 @@ class RegistrosActivity : AppCompatActivity() {
         etEmail = findViewById(R.id.et_email)
         etTelefono = findViewById(R.id.et_Telefono)
         etPass = findViewById(R.id.et_Pass)
-
         etId = findViewById(R.id.et_Id)
-
         id = intent.getStringExtra("id").toString()
-
         etId.setText(id)
-
         cargarDatos()
 
 
+    }
+
+    fun limpiarCampos(){
+        etNombre.setText("")
+        etEmail.setText("")
+        etTelefono.setText("")
+        etPass.setText("")
+        etId.setText("")
     }
 
     fun cargarDatos(){
@@ -60,7 +66,50 @@ class RegistrosActivity : AppCompatActivity() {
     }
 
     fun eliminar(Vista: View){
+        val url = "http://192.168.1.3/android_mysql2/borrar.php"
+        procesoEnCola = Volley.newRequestQueue(this)
+        var resultadoPost = object : StringRequest(Request.Method.POST, url,
+            Response.Listener { Respuesta->
+                limpiarCampos()
+                Toast.makeText(this,"El usuario a sido borrado",Toast.LENGTH_LONG).show()
+            }, Response.ErrorListener { error->
+                Toast.makeText(this,"Error al borrar usuario",Toast.LENGTH_LONG).show()
+            }){
+            override fun getParams(): MutableMap<String, String>? {
+                val parametros = HashMap<String,String>()
+                parametros.put("id",etId.text.toString())
+                return parametros
+            }
+        }
+        procesoEnCola.add(resultadoPost)
+    }
 
+    fun editar(Vista: View){
+        val url = "http://192.168.1.3/android_mysql2/editar.php"
+        procesoEnCola = Volley.newRequestQueue(this)
+        val resultadoPost = object : StringRequest(Request.Method.POST,url,
+            Response.Listener { respuesta->
+                Toast.makeText(this,"El usuario fue editado correctamente",Toast.LENGTH_LONG).show()
+            },Response.ErrorListener { error->
+                Toast.makeText(this,"Error al editar usuario",Toast.LENGTH_LONG).show()
+            }){
+            override fun getParams(): MutableMap<String, String>? {
+                val parametros = HashMap<String,String>()
+                parametros.put("id",etId.text.toString())
+                parametros.put("nombre",etNombre.text.toString())
+                parametros.put("email",etEmail.text.toString())
+                parametros.put("telefono",etTelefono.text.toString())
+                parametros.put("pass",etPass.text.toString())
+                return parametros
+            }
+        }
+        procesoEnCola.add(resultadoPost)
+
+    }
+
+    fun regresar(Vista: View){
+        var ventana = Intent(this,MainActivity::class.java)
+        startActivity(ventana)
     }
 
 
